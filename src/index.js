6faa567b928031f093e1d8d7f8da7f47f83071b8
Vittorio <vittfassano@gmail.com>
2022-11-11 14:44:41 -0300

    feat: connection with database

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ app.use(cors());
 dotenv.config();
 app.use(express.json());
 
+//connection with mongo
 const mongoClient = new MongoClient(process.env.MONGO_URI);
+const port = process.env.PORT;
 
 try {
     await mongoClient.connect();
@@ -18,7 +20,20 @@ try {
     console.log(err);
 };
 
+//connection with database
+const database = process.env.MONGO_DB;
+let db = null;
 
-app.listen(process.env.PORT, () => {
-    console.log(chalk.bold.green(`Server running in port: ${process.env.PORT}`));
+try {
+    db = mongoClient.db(database);
+} catch (err) {
+    console.log("Error to connect", err);
+};
+
+//schemasJoi
+
+
+//turn on the server
+app.listen(port, () => {
+    console.log(chalk.bold.green(`Server running in port: ${port}`));
 });
