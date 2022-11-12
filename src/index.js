12f78f309e34bf3e931a4014c4ab5a627597115f
Vittorio <vittfassano@gmail.com>
2022-11-12 09:58:23 -0300

    feat: post participants v01

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import chalk from 'chalk';
 import dotenv from 'dotenv';
 import {MongoClient} from 'mongodb';
+import Joi from 'joi';
 
 const app = express();
 app.use(cors());
@@ -31,7 +32,30 @@ try {
 };
 
 //schemasJoi
+const participantSchema = Joi.object ({
+    name: Joi.string().required(),
+    lastStatus: Joi.number().integer()
+});
 
+//route post participants
+app.post("/participants", async (req, res) => {
+    const {name} = req.body;
+
+    try {
+        const validatingUser = participantSchema.validate({name}, {abortEarly: false});
+
+        await db.collection("participants").insertOne({name, lastStatus: Date.now()});
+
+        res.sendStatus(201);
+        console.log("Validated user");
+        return;
+
+    } catch (err) {
+        res.sendStatus(422);
+        console.log(err);
+        return;
+    };
+});
 
 //turn on the server
 app.listen(port, () => {
