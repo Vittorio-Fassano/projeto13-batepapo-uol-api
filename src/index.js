5ce8f37b671e499b93f034fc0aeb4b10faf5381f
Vittorio <vittfassano@gmail.com>
2022-11-12 15:38:06 -0300

    feat: post participants (messages) v02

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import chalk from 'chalk';
 import dotenv from 'dotenv';
 import {MongoClient} from 'mongodb';
 import Joi from 'joi';
+import dayjs from 'dayjs';
 
 //general configs
 const app = express();
@@ -39,6 +40,14 @@ const participantSchema = Joi.object ({
     lastStatus: Joi.number().integer()
 });
 
+const messageSchema = Joi.object ({
+    from: Joi.string().required(),
+    to:   Joi.string().required(),
+    text: Joi.string().required(),
+    type: Joi.string().required().valid("message", "private_message"),
+    time: Joi.string()
+});
+
 //route post participants
 app.post("/participants", async (req, res) => {
     const {name} = req.body;
@@ -48,34 +57,54 @@ app.post("/participants", async (req, res) => {
         const userAlreadyExist = await db.collection("participants").findOne({name});
 
         if (validatingUser.error || userAlreadyExist !== null ) {
-            res.status(409).send("Invalid user");
+            res.status(409).send("Invalid user");//error validating a user
             console.log(validatingUser.error.details.map((detail) => detail.message));
             return;
 
         } else {
+            //insert users
             await db.collection("participants").insertOne({name, lastStatus: Date.now()});
+
+            //insert initial messages
+            await db.collection("messages").insertOne({
+                from: name, 
+                to: "Todos", 
+                text: "entra na sala...", 
+                type: "status",  
+                time: dayjs(Date.now()).format("HH:mm:ss")
+            });
+
+            //response
             res.sendStatus(201);
             console.log("Validated user");
             return;
         };
         
     } catch (err) {
-        res.sendStatus(422);
-        console.log(err);
+        res.sendStatus(422); //error registering a user
         return;
     };
 });
+
+//route delete participants (TO DO)
+
 
 //route get participants
 app.get("/participants", async(req, res) => {
 
     try {
         const users = await db.collection("participants").find({}).toArray();
+
+        //show initial messages
+        const showInitialMessages = await db.collection("messages").find({}).toArray();
+        console.log(showInitialMessages);
+        //
+        
         res.status(200).send(users);
         return;
 
     } catch(err) {
-        res.sendStatus(500);
+        res.sendStatus(500); //error accessing users
         return;
     };
 });
